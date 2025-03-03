266e4baaa57fd6ee2c129307e75d092fad77d088
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2025-03-03 14:21:08 -0500

    Exec file first iter

---
+++ src/core/mydbexec.h
@@ -0,0 +1,18 @@
+#ifndef MYDBEXEC_H
+#define MYDBEXEC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mydb.h"
+
+int mydb_exec(
+    mydb*,                                  /* An open database */
+    const char *sql,                           /* SQL to be evaluated */
+    int (*callback)(void*,int,char**,char**),  /* Callback function */
+    void *,                                    /* 1st argument to callback */
+    char **errmsg                              /* Error msg written here */
+  );
+
+
+
+#endif // MYDBEXEC_H
