94abc67a27dd500d26aea83b8db35c61385d172a
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2025-03-05 13:17:00 -0500

    Call back implemented for SELECT statements | .tables not working  | Create db first and then table CHECK

--- src/core/mydb.h
+++ src/core/mydb.h
@@ -1,30 +1,3 @@
-// #ifndef MYDB_H
-// #define MYDB_H
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// // Creating a db connection handle struct
-// typedef struct mydb{
-//     FILE *db_file;  // File pointer for database storage
-//     char *filename; // Stores the database filename 
-// } mydb_t;
-
-// // mydb_t is actually a shorthand for struct mydb
-
-
-// int mydb_open(
-//     const char *filename,
-//     mydb_t **db
-// );
-
-// int mydb_close(
-//     mydb_t *db
-// );
-
-// #endif // MYDB_H
-
-
 #ifndef MYDB_H
 #define MYDB_H
 
--- src/main.c
+++ src/main.c
@@ -4,33 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_SIZE 1024  // Max SQL command length
+#define MAX_INPUT_SIZE 1024
+
+// Callback to print SELECT results
+static int print_results(void *unused, int num_columns, char **column_values, char **column_names) {
+    (void)unused;
+    for (int i = 0; i < num_columns; i++) {
+        printf("%-15s", column_names[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < num_columns; i++) {
+        printf("%-15s", column_values[i] ? column_values[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
 
 void start_repl(mydb_t *db) {
     char input[MAX_INPUT_SIZE];
     char *errmsg = NULL;
 
-    printf("Welcome to mydb! Type SQL commands or '.exit' to quit.\n");
-
     while (1) {
-        printf("mydb> ");  // REPL prompt
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-            printf("\nExiting...\n");
-            break;
-        }
-
-        // Remove newline from input
+        printf("mydb> ");
+        fgets(input, MAX_INPUT_SIZE, stdin);
         input[strcspn(input, "\n")] = 0;
 
-        if (strcmp(input, ".exit") == 0) {
-            printf("Goodbye!\n");
-            break;
+        if (strcmp(input, ".exit") == 0) break;
+
+        // Use callback ONLY for SELECT-like queries
+        int (*callback)(void*,int,char**,char**) = NULL;
+        if (strncasecmp(input, "SELECT", 6) == 0) {
+            callback = print_results; // Use your print_results callback
         }
 
-        // Execute the command
-        if (mydb_exec(db, input, NULL, NULL, &errmsg) != 0) {
-            printf("SQL Error: %s\n", errmsg);
-            sqlite3_free(errmsg);  // Free SQLite error message
+        if (mydb_exec(db, input, callback, NULL, &errmsg) != 0) {
+            printf("Error: %s\n", errmsg);
+            sqlite3_free(errmsg);
         }
     }
 }
@@ -38,16 +47,12 @@ void start_repl(mydb_t *db) {
 int main() {
     mydb_t *db;
 
-    // Open database
     if (mydb_open("test.db", &db) != 0) {
         printf("Failed to open database.\n");
         return 1;
     }
 
-    // Start the interactive REPL
     start_repl(db);
-
-    // Close database
     mydb_close(db);
 
     return 0;
